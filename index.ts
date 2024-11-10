aac7a8c909f650a8c2cd7f88c97f5f3c594ce9ba
WcaleNieWolny <michael@capgo.app>
2024-11-10 11:50:00 +0000

    feat: google offline access

--- index.ts
+++ index.ts
@@ -7,6 +7,10 @@ import jsonwebtoken from 'jsonwebtoken'
 import { logger } from 'hono/logger'
 import { cors } from 'hono/cors'
 import jwkToPem from 'jwk-to-pem'
+import { zValidator } from '@hono/zod-validator'
+import { z } from 'zod'
+import { jwt, sign } from 'hono/jwt'
+
 
 const app = new Hono()
 app.use(logger())
@@ -130,6 +134,156 @@ app.post('/login/callback', async (c) => {
   } else {
     return c.redirect(`${BASE_REDIRECT}?success=true&ios_no_code=true`)
   }
+})
+
+// userInfoZodSchema and googleExchangeZodSchema are not extenisive
+const googleExchangeZodSchema = z.object({
+  access_token: z.string(),
+  expires_in: z.number(),
+  refresh_token: z.string().optional(),
+})
+
+const userInfoZodSchema = z.object({
+  email: z.string()
+})
+
+const googleDb = await JSONFilePreset('google_db.json', { users: [] as { email: string, refresh_token: string, access_token: string }[] })
+
+
+app.use('/auth/google_offline', cors())
+app.post(
+  '/auth/google_offline',
+  zValidator(
+    'json',
+    z.object({
+      serverAuthCode: z.string(),
+      platform: z.enum(["android", "ios", "web"])
+    })
+  ),
+  async (c) => {
+    const { serverAuthCode, platform } = c.req.valid('json')
+    const url = new URL('https://oauth2.googleapis.com/token')
+    const params = url.searchParams
+
+    params.set("code", serverAuthCode)
+    params.set('client_id', process.env.GOOGLE_CLIENT_ID ?? '')
+    params.set('client_secret', process.env.GOOGLE_CLIENT_SECRET ?? '')
+    params.set('redirect_uri', platform === 'web' ? 'postmessage' : '')
+    params.set('grant_type', 'authorization_code')
+
+    console.log(url)
+
+    // exchange serverAuthCode for access_token and refresh token
+    const googleResponse = await fetch(url, {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      method: 'POST'
+    })
+    
+    // verify the response
+    if (googleResponse.status !== 200) {
+      console.log(`Google response returned an invalid status. Stauts: ${googleResponse.status} Text: ${await googleResponse.text()}`)
+      c.json({ error: 'google_exchange_error' }, 500)
+      return
+    }
+
+    const rawBody = await googleResponse.json()
+    const parsed = googleExchangeZodSchema.safeParse(rawBody)
+    if (!parsed.success) {
+      console.log('Cannot parse exchange response', parsed.error)
+      c.json({ error: 'google_exchange_error_parse' }, 500)
+      return
+    }
+
+    const access = parsed.data
+    // fetch the userinfo
+    const userinfoResponse = await fetch('https://openidconnect.googleapis.com/v1/userinfo', {
+      headers: {
+        "Authorization": `Bearer ${access.access_token}`,
+        "Content-Type": "application/json"
+      }
+    })
+
+    if (userinfoResponse.status !== 200) {
+      console.log(`Google userinfo returned an invalid status. Stauts: ${userinfoResponse.status} Text: ${await userinfoResponse.text()}`)
+      c.json({ error: 'google_user_info_error' }, 500)
+      return
+    }
+
+    const parsedUserInfo = userInfoZodSchema.safeParse(await userinfoResponse.json())
+    if (!parsedUserInfo.success) {
+      console.log('Cannot parse user info', parsedUserInfo.error)
+      c.json({ error: 'google_userinfo_error_parse' }, 500)
+      return
+    }
+
+    const { email } = parsedUserInfo.data
+    const user = googleDb.data.users.find(usr => usr.email === email)
+    if (user) {
+      const refreshToken = access.refresh_token ?? user.refresh_token
+      if (user.refresh_token != refreshToken) {
+        user.refresh_token = refreshToken
+      }
+      user.access_token = access.access_token
+      await googleDb.write()
+    } else {
+      if (!access.refresh_token) {
+        // this should never happen
+        console.error("User not found, and refresh token was not provided?")
+        c.json({ error: 'user_not_found_and_no_refresh_token' }, 500)
+        return
+      }
+      googleDb.data.users.push({
+        email: email,
+        access_token: access.access_token,
+        refresh_token: access.refresh_token ?? '' 
+      })
+      await googleDb.write()
+    }
+
+    const jwt = await sign({
+      sub: email,
+      exp: Math.floor(Date.now() / 1000) + access.expires_in,
+    }, process.env.CUSTOM_JWT_SECRET ?? '')
+
+    return c.json({ jwt })
+    // ... use your validated data
+  }
+)
+
+app.use('/auth/get_google_user', cors())
+app.use(
+  '/auth/get_google_user',
+  jwt({
+    secret: process.env.CUSTOM_JWT_SECRET ?? '',
+  })
+)
+app.get('/auth/get_google_user', async (c) => {
+  const email = c.get('jwtPayload').sub
+  if (email === null) {
+    return c.json({error: 'invalid_request'}, 400);
+  }
+
+  const user = googleDb.data.users.find(usr => usr.email === email)
+  if (!user) {
+    return c.json({ error: 'user_not_found' }, 500)
+  }
+
+  const userinfoResponse = await fetch('https://openidconnect.googleapis.com/v1/userinfo', {
+    headers: {
+      "Authorization": `Bearer ${user.access_token}`,
+      "Content-Type": "application/json"
+    }
+  })
+
+  if (userinfoResponse.status !== 200) {
+    console.log(`Google userinfo returned an invalid status. Stauts: ${userinfoResponse.status} Text: ${await userinfoResponse.text()}`)
+    c.json({ error: 'google_user_info_error' }, 500)
+    return
+  }
+
+  return c.text(await userinfoResponse.text())
 })
 
 app.post('/auth/google_validation', async (c) => {
